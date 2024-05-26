70340fb9dbe0623e80249aba32689b4ef8306fea
elahipour <sefrtabist@gmail.com>
2024-05-26 09:44:57 -0700

    Fixing the problem of duplicate keywords

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -1,53 +1,48 @@
-const wrapper = document.querySelector("#wrapper")
-const bad = document.querySelector("#bad");
-const container = document.querySelector("#container")
+const container = document.querySelector("#container");
+const bad = document.querySelector("#bad-word");
+const result = document.querySelector("#container>div:last-child");
 const badsList = document.querySelector("#badsList");
+
 let bads = [];
 let badsInText = [];
 let noRepeated = [];
+
 const Save = () => {
-
-    bads.push(bad.value);
-    const li = document.createElement('li');
-    li.innerHTML = bad.value;
-    badsList.appendChild(li);
-    noRepeated = new Set(bads)
-    bad.value='';
-    bad.focus();
-
-}
-let splited = '';
-let stars = '';
+  const isFind = bads.find((word) => word === bad.value);
+  if (isFind) return;
+  bads.push(bad.value);
+  const li = document.createElement("li");
+  li.innerHTML = bad.value;
+  badsList.appendChild(li);
+  noRepeated = new Set(bads);
+  bad.value = "";
+  bad.focus();
+};
+let splited = "";
+let stars = "";
 const text = document.querySelector("#text");
 const checkString = (e) => {
-    container.innerHTML = text.value;
-    splited = container.innerHTML.split(" ");
-    console.log(splited)
-    bads = [...noRepeated];
-    badsInText = [];
+  result.innerHTML = text.value;
+  splited = result.innerHTML.split(" ");
+  bads = [...noRepeated];
+  badsInText = [];
 
-    for (let badWord of bads) {
-        for (let i of splited) {
-            if (badWord === i) {
-                badsInText.push(i);
-                for (let j = 0; j < i.length; j++) {
-                    stars += '*'
-                    //😲
-                }
-                const result = container.innerText.replace(i, stars)
-                container.innerHTML = result;
-                stars = '';
-            }
+  for (let badWord of bads) {
+    for (let i of splited) {
+      if (badWord === i) {
+        badsInText.push(i);
+        for (let j = 0; j < i.length; j++) {
+          stars += "*";
+          //😲
         }
+        const res = result.innerText.replace(i, stars);
+        result.innerHTML = res;
+        stars = "";
+      }
     }
-}
+  }
+};
 
 window.addEventListener("load", function () {
-    wrapper.style.opacity = "1";
-})
-
-
-
-
-
-
+  container.style.opacity = "1";
+});
